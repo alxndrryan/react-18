89f6893503a3ce7651affcb417ae30c6988d2f61
Alexander Hogle <alexanderhogle@gmail.com>
2023-11-08 17:57:27 -0600

    click button to show and dismiss alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
+import Alert from './components/Alert';
 import ListGroup from './components/ListGroup';
+import Button from './components/Button';
+import { useState } from 'react';
 
 function App() {
-  let items = ['New York', 'San Fransico', 'Tokyo', 'London', 'Paris'];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
