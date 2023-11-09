59ba087dd5f0b1e6aa27de84a727ab46a5b88313
Alexander Hogle <alexanderhogle@gmail.com>
2023-11-08 18:13:09 -0600

    list styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,11 @@ import Button from './components/Button';
 import { useState } from 'react';
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
+  const items = ['New York', 'Los Angeles', 'San Francisco'];
+
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
-      )}
-      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
     </div>
   );
 }
