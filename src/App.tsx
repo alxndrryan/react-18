92e1614d96789352c0bb11e0dfcdaee99edef2bb
Alexander Hogle <alexanderhogle@gmail.com>
2023-11-10 16:02:13 -0600

    builds an expandable text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
-import Alert from './components/Alert';
-import ListGroup from './components/ListGroup';
-import Button from './components/Button/Button';
 import { useState } from 'react';
 
-import { BsFillCalendarFill } from 'react-icons/bs';
-import Like from './components/Like';
-import NavBar from './components/NavBar';
-import Cart from './components/Cart';
+import ExpandableText from './components/ExpandableText';
 
 function App() {
-  const [cartItems, setCartItems] = useState(['Product1', 'Product2']);
-
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+      <ExpandableText>Hello World</ExpandableText>
     </div>
   );
 }
