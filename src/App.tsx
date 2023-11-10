0a5c2ac77b466f6298e403b53fe7f20d80da6dd1
Alexander Hogle <alexanderhogle@gmail.com>
2023-11-09 19:48:17 -0600

    practicing styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import Alert from './components/Alert';
 import ListGroup from './components/ListGroup';
-import Button from './components/Button';
+import Button from './components/Button/Button';
 import { useState } from 'react';
+
+import { BsFillCalendarFill } from 'react-icons/bs';
 
 function App() {
   const items = ['New York', 'Los Angeles', 'San Francisco'];
 
   return (
     <div>
-      <ListGroup heading="Miami" items={items} onSelectItem={() => {}} />
+      <BsFillCalendarFill color="red" size="40" />
+      <Button onClick={() => {}}>My Button</Button>
     </div>
   );
 }
